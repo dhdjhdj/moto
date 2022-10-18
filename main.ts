96d895472892798aa8d80be8697d8bfe278d0fe8
dhdjhdj <112738291+dhdjhdj@users.noreply.github.com>
2022-10-18 08:59:15 +0800

    Add files via upload

--- main.ts
+++ main.ts
@@ -1,4 +1,7 @@
 basic.forever(function () {
     Motor.motor(Motorlist.M1, Direction1.Backward, 200)
     Motor.motor(Motorlist.M2, Direction1.Forward, 200)
+    LedRgb.rgb_led(LED_rgb_L_R.LED_L, 255, 255, 255)
+    LedRgb.rgb_led(LED_rgb_L_R.LED_R, 255,255,255)
 })
+
--- motordrive.ts
+++ motordrive.ts
@@ -73,41 +73,39 @@ enum LED_rgb_L_R
     LED_L = 0,
 }
 
-//% color="#AA278D"
-namespace Motor {
+// //% color="#AA278D"
+// namespace Motor {
 
-    //% block="motor = | %motor Direction = | $direction speed = $pwmvalue"
-    //% direction.shadow=timePicker
-    //% pwmvalue.min=0 pwmvalue.max=255 
-    export function motor(motor: Motorlist, direction: Direction1, pwmvalue: number) {
-        switch(motor){
-            case 1: // M1电机控制
-                if (direction) { motor_i2cWrite(0x01,pwmvalue); motor_i2cWrite(0x02, 0);}
-                else { motor_i2cWrite(0x02,pwmvalue); motor_i2cWrite(0x01, 0); }
-                break;
-            case 2: // M2电机控制
-                if (direction) { motor_i2cWrite(0x04,pwmvalue); motor_i2cWrite(0x03, 0); }
-                else { motor_i2cWrite(0x03,pwmvalue); motor_i2cWrite(0x04, 0); }
-                break;
-        }
-    }
+//     //% block="motor = | %motor Direction = | $direction speed = $pwmvalue"
+//     //% direction.shadow=timePicker
+//     //% pwmvalue.min=0 pwmvalue.max=255 
+//     export function motor(motor: Motorlist, direction: Direction1, pwmvalue: number) {
+//         switch(motor){
+//             case 1: // M1电机控制
+//                 if (direction) { motor_i2cWrite(0x01,pwmvalue); motor_i2cWrite(0x02, 0);}
+//                 else { motor_i2cWrite(0x02,pwmvalue); motor_i2cWrite(0x01, 0); }
+//                 break;
+//             case 2: // M2电机控制
+//                 if (direction) { motor_i2cWrite(0x04,pwmvalue); motor_i2cWrite(0x03, 0); }
+//                 else { motor_i2cWrite(0x03,pwmvalue); motor_i2cWrite(0x04, 0); }
+//                 break;
+//         }
+//     }
 
 //% color="#7f009f"
-namespace RGB {
+namespace Ledrgb {
    
-    /**
-     * This is a statement block with a parameter
-     */
-    
     //% block="RGB = |%place rad   = $arg1 green =$arg2 blue  = $arg3"
     export function led_rgb(place:LED_rgb_L_R,arg1: number, arg2: number,arg3: number) {
          switch(place){
             case 0: {motor_i2cWrite(0x09, arg1);
                      motor_i2cWrite(0x0a, arg2);
-                     motor_i2cWrite(0x0b, arg3);} break;
+                     motor_i2cWrite(0x0b, arg3);} 
+                     break;
             case 1: {motor_i2cWrite(0x0c, arg1);
                      motor_i2cWrite(0x0d, arg2);
-                     motor_i2cWrite(0x0e, arg3);} break;
+                     motor_i2cWrite(0x0e, arg3);} 
+                     break;
          }
 
     }
